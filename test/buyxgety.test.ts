edc0f4bd69241278f7ae28eac2b637ca1b6456cc
Kittiphat S <freeuxer@gmail.com>
2021-02-02 23:34:20 +0700

    added buyxgety test cases

--- test/buyxgety.test.ts
+++ test/buyxgety.test.ts
@@ -98,4 +98,84 @@ describe('Calculation Engine', () => {
     }
     expect(result).toEqual({ input, meta })
   })
+
+  it('Can create pick-3-pay-2 rule for uid TEST AND buy-1-get-1 for UID TEST2 in Cart.', async () => {
+    const uid1 = '0001'
+    const buyXGetY_32 = new BuyXGetYRule(uid1, 0, 'buyXGetY_32', 
+    [
+      {
+        type: 'uids',
+        uids: ['TEST'],
+      },
+    ], 2, 1)
+    const uid2 = '0002'
+    const buyXGetY_11 = new BuyXGetYRule(uid2, 1, 'buyXGetY_11', 
+    [
+      {
+        type: 'uids',
+        uids: ['TEST2'],
+      },
+    ], 1, 1)
+
+    const input = {
+      items: [
+        {
+          uid: 'TEST',
+          cartItemIndexKey: '0',
+          qty: 6,
+          perItemPrice: 145,
+          categories: ['Main'],
+          tags: ['TAG#1'],
+        },
+        {
+          uid: 'TEST2',
+          cartItemIndexKey: '1',
+          qty: 6,
+          perItemPrice: 990,
+          categories: ['Main'],
+          tags: ['TAG#1'],
+        }
+      ],
+      rules: [buyXGetY_32, buyXGetY_11],
+    }
+
+    const result = await engine.process(input, {})
+
+    const meta = {
+      applicableRuleUids: [uid1, uid2],
+      itemDiscounts: [
+        {
+          uid: 'TEST',
+          perLineDiscountedAmount: 145,
+          setFree: true,
+          applicableRuleUid: '0001',
+        },
+        {
+          uid: 'TEST',
+          perLineDiscountedAmount: 145,
+          setFree: true,
+          applicableRuleUid: '0001',
+        },
+        {
+          uid: 'TEST2',
+          perLineDiscountedAmount: 990,
+          setFree: true,
+          applicableRuleUid: '0002',
+        },
+        {
+          uid: 'TEST2',
+          perLineDiscountedAmount: 990,
+          setFree: true,
+          applicableRuleUid: '0002',
+        },
+        {
+          uid: 'TEST2',
+          perLineDiscountedAmount: 990,
+          setFree: true,
+          applicableRuleUid: '0002',
+        }
+      ],
+    }
+    expect(result).toEqual({ input, meta })
+  })
 })
